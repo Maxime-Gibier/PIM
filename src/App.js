e34b762cae1967c65d375bacd48cb1dd5c93b439
Maxime <maxime.gibier@icloud.com>
2022-04-20 13:25:21 +0200

    search filters

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import './App.css';
-import Header from "./Components/Header/Header"
-import Search from './Components/Search/Search';
-import DerivedProduct from './Components/derivedProduct/derivedProduct';
-import Gallery from "./Components/Gallery/Gallery"
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Search from "./Components/Search/Search";
+import DerivedProduct from "./Components/derivedProduct/derivedProduct";
+import Gallery from "./Components/Gallery/Gallery";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
+
+
+	return (
+		<div className="App">
+			<Header />
       <Search />
-      <DerivedProduct />
-      <Gallery />
-    </div>
-  );
+			<DerivedProduct />
+			<Gallery />
+		</div>
+	);
 }
 
 export default App;
--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,49 +1,88 @@
-import React from 'react';
-import "./Search.css"
+import React, { useState } from "react";
+import "./Search.css";
 
 const Search = () => {
-    return (
-			<div class="search__div">
-				<form>
-					<input type="string"></input>
-					<div class="filter">
-						<div>
-							<button name="Filtre">Categories</button>
-							<div class="filtres" id="Categories">
-								<button></button>
-								<button></button>
-								<button></button>
-							</div>
-						</div>
-						<div>
-							<button name="Filtre">Prix</button>
-							<div class="filtres">
-								<button></button>
-								<button></button>
-								<button></button>
-							</div>
-						</div>
-						<div>
-							<button name="Filtre">Plateforme</button>
-							<div class="filtres">
-								<button></button>
-								<button></button>
-								<button></button>
-							</div>
-						</div>
-						<div>
-							<button name="Filtre">Filtre</button>
-							<div class="filtres">
-								<button></button>
-								<button></button>
-								<button></button>
-							</div>
-						</div>
+	const [activeFilter, setActiveFilter] = useState("");
+	console.log(activeFilter);
+	return (
+		<div class="search__div">
+			<form>
+				<input type="string"></input>
+			</form>
+			<div className="filter">
+				<div>
+					<button
+						name="Filtre"
+						onClick={(event) =>
+							activeFilter === "Categories"
+								? setActiveFilter("")
+								: setActiveFilter("Categories")
+						}
+					>
+						Categories
+					</button>
+					<div className={activeFilter === "Categories" ? "active" : "filtres"}>
+						<button></button>
+						<button></button>
+						<button></button>
 					</div>
-					<button type="Submit">Chercher</button>
-				</form>
+				</div>
+				<div>
+					<button
+						name="Filtre"
+						onClick={(event) =>
+							activeFilter === "Prix"
+								? setActiveFilter("")
+								: setActiveFilter("Prix")
+						}
+					>
+						Prix
+					</button>
+					<div className={activeFilter === "Prix" ? "active" : "filtres"}>
+						<button></button>
+						<button></button>
+						<button></button>
+					</div>
+				</div>
+				<div>
+					<button
+						name="Filtre"
+						onClick={(event) =>
+							activeFilter === "Plateforme"
+								? setActiveFilter("")
+								: setActiveFilter("Plateforme")
+						}
+					>
+						Plateforme
+					</button>
+					<div className={activeFilter === "Plateforme" ? "active" : "filtres"}>
+						<button></button>
+						<button></button>
+						<button></button>
+					</div>
+				</div>
+				<div>
+					<button
+						name="Filtre"
+						onClick={(event) =>
+							activeFilter === "DLC"
+								? setActiveFilter("")
+								: setActiveFilter("DLC")
+						}
+					>
+						DLC
+					</button>
+					<div className={activeFilter === "DLC" ? "active" : "filtres"}>
+						<button></button>
+						<button></button>
+						<button></button>
+					</div>
+				</div>
 			</div>
-		);
+			<button type="Submit">Chercher</button>
+		</div>
+	);
+	console.log(activeFilter);
 };
 
 export default Search;
