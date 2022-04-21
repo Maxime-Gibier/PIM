0f0a3d50621355dc4052586f11cc443889b486db
Maxime <maxime.gibier@icloud.com>
2022-04-21 10:12:36 +0200

    un peu de style sans branch encore une fois

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,8 +5,10 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
 	return (
-		<div>
-			<Link to="/"><h2>Home</h2></Link>
+		<div className="header">
+			<Link to="/" className="home">
+				<h2>Home</h2>
+			</Link>
 			<input type="checkbox" id="menu-toggle"></input>
 			<label for="menu-toggle" className="hamburger">
 				<span className="bun bun-top">
